98b280b2414af33edb187abd5959e0a90bb649cd
Kaitlin Swanson <kateswanson94@gmail.com>
2021-05-27 00:06:21 -0700

    Add script.js

--- script.js
+++ script.js
@@ -1,39 +1,119 @@
-// create a new function called operate. 
-// takes an operator and 2 numbers and then calls one of the (add, sub, mult, div) functions on the numbers. 
-function operate(number1, operator, number2) {
+// assign button categories/constants
 
-if (operator === '+') { 
-    return number1 + number2; 
-}
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const screen = document.querySelector("[data-screen]");
 
-if (operator === '-') { 
-    return number1 - number2; 
-}
-
-if (operator === 'x') { 
-    return number1 * number2;
-}
-
-if (operator === '/') { 
-    return number1 / number2;
-} else { 
-    return 'ERROR';
-}
-}; 
-
-//go back and create a basic HTML calculator with digit buttons and = key and display
-//come back to do the JS later for it. 
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
 
 
+equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clear);
 
 // Create the functions that populate the display when you click the number buttons… you should be storing the ‘display value’ in a variable somewhere for use in the next step.
 
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => setOperation(button.textContent))
+);
+
+function appendNumber(number) {
+  if (screen.textContent === "0" || shouldResetScreen) resetScreen();
+  screen.textContent += number;
+}
+
+function resetScreen() {
+  screen.textContent = "";
+  shouldResetScreen = false;
+}
+
+function clear() {
+  screen.textContent = "0";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
+}
+
+function appendPoint() {
+  if (shouldResetScreen) resetScreen();
+  if (screen.textContent === "") screen.textContent = "0";
+  if (screen.textContent.includes(".")) return;
+  screen.textContent += ".";
+}
+
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate();
+  firstOperand = screen.textContent;
+  currentOperation = operator;
+  shouldResetScreen = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === "÷" && screen.textContent === "0") {
+    alert("Nope! You can't divide by 0!");
+    clear();
+    return;
+  }
+  secondOperand = screen.textContent;
+  screen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  currentOperation = null;
+}
+// operate function - what to do with num1, num2 and operator. 
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+// takes an operator and 2 numbers and then calls one of the (add, sub, mult, div) functions on the numbers. 
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
 
 
 
-//Make the calculator work! You’ll need to store the first number that is input into the calculator when a user presses
-// an operator, and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
-// You should already have the code that can populate the display, so once operate() has been called, update the display with 
-// the ‘solution’ to the operation.
-// You need to figure out how to store all the values and call the operate function with them. 
+
+
+
 
